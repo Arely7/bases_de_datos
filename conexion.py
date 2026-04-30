4376d66d6565820786ef2984528d8d1e0d36fde9
Arely7 <jrp678179@gmail.com>
2026-04-30 14:56:11 -0600

    avance

---
+++ conexion.py
@@ -0,0 +1,32 @@
+import mysql.connector
+from mysql.connector import Error
+
+class Conexion:
+    def __init__(self):
+        self.config = {
+            "host":'localhost', #o '127.0.0.1'
+            "user":"root",
+            "password":"",
+            "db":"prueba_pyhton"
+        }
+        self.conexion = None
+        self.cursor = None
+
+def conectar(self):
+    try:
+        self.conexion = mysql.connector.connect(**self.config)
+        if self.conexion.is_connected():
+            print("Conexión exitosa")
+            self.cursor = self.conexion.cursor()
+    except Error as e:
+        print(f"ERROR: {e}")
+
+#sirve para insertar, modificar o eliminar
+def insertar(self,sql,valores):
+        self.cursor.execute(sql,valores)
+        self.conexion.commit()
+#sirve para seleccionar
+def seleccionar(self,sql):
+        self.cursor.execute(sql)
+        resultado = self.cursor.fetchall()
+        return resultado
---
+++ main_controller.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt6 import QtWidgets, uic
+
+class MainController(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("main.ui",self)
+
+        self.btn_insertar.connect(self.add_user)
+    
+    def add_user(self):
+        name = self.txt_nombre.text()
+        last = self.txt_apellido.text()
+        email = self.txt_email.text()
+        passw = self.txt_password.text()
+        passw_confirm = self.txt_confirmar.text()
+
+        if name.strip() == "" or last.strip() == "" or email.strip() == "" or passw.strip() == "" or passw_confirm.strip() == "":
+            QtWidgets.QMessageBox.warning(self, "Favor de llenar todos los campos")
+        elif passw.strip() != passw_confirm.strip():
+            QtWidgets.QMessageBox.warning(self,"Las contraseñas no coinciden")
+        else:
+            print("")
